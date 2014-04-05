46b3313fc4647a7db956bf84330d308f42301689
udantha <udanthaya@gmail.com>
2014-04-05 23:07:23 +0530

    Two layouts initiated.

---
+++ traxytaxi-server/lib/router.js
@@ -0,0 +1,14 @@
+/**
+ * Created by udantha on 4/5/14.
+ */
+Router.configure({
+    // layoutTemplate: 'layout'
+});
+
+Router.map(function(){
+    this.route('taxiList', {path: '/'});
+
+    this.route('taxiPage', {
+        path: '/taxiapp'
+    })
+})
