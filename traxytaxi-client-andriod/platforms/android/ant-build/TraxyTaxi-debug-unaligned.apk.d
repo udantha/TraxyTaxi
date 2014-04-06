b8412e139c326ad8141e6feff36397c9c4f9f13e
Vipula S <v.s.anandapiya@gmail.com>
2014-04-06 09:30:48 +0530

    # Employee app added

---
+++ traxytaxi-client-andriod/platforms/android/ant-build/TraxyTaxi-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\TraxyTaxiMeteor\traxytaxi-client-andriod\platforms\android\ant-build\TraxyTaxi-debug-unaligned.apk : \
+D:\TraxyTaxiMeteor\traxytaxi-client-andriod\platforms\android\ant-build\TraxyTaxi.ap_ \
+D:\TraxyTaxiMeteor\traxytaxi-client-andriod\platforms\android\ant-build\classes.dex \
+D:\TraxyTaxiMeteor\traxytaxi-client-andriod\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ traxytaxi-employee-andriod/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\TraxyTaxiMeteor\traxytaxi-employee-andriod\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+D:\TraxyTaxiMeteor\traxytaxi-employee-andriod\platforms\android\CordovaLib\AndroidManifest.xml \
